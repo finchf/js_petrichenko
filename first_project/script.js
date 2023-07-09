ddffa049cdb0023a90fff476586671c74e98411d
Dmitrij <dmbor123@gmail.com>
2023-07-09 14:48:57 +0300

    3 commit

--- first_project/script.js
+++ first_project/script.js
@@ -8,12 +8,27 @@ const personalMovieDB = {
     privat: false
 }
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('на сколько оцените этот фильм?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('на сколько оцените этот фильм?', '')
+for (let i  = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('на сколько оцените этот фильм?', '');
 
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
+    if (a != null && b != null && a != '' & b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b
+    } else {
+        console.log('error')
+        i--
+    }
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов')
+    } else if (personalMovieDB.count>= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель')
+    } else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман')
+    }
+
+
+}
+
 
 console.log(personalMovieDB)
